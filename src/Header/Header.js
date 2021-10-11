7feec7454e710c22fa1c60d264e04df9c7980424
Leonardo Borges <beolorges@gmail.com>
2021-10-11 16:17:11 -0300

    Header com logo e botões css incompleto

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,19 +6,19 @@ function Header(){
 
         <div className = "container">
             
-            <div className="logo">
+            <div className = "base">
+                <div className="logo">
                     <img src="./images/logo_header.png" alt= "Stinho"/> 
                 </div>
 
-            <div className = "base">
                 <div className = "botoes">
-                    <div className = "home"><button>Home</button></div>
-                    <div className = "store"><button>Store</button></div>
-                    <div className = "biblioteca"><button>Biblioteca</button></div>
-                    <div className = "entrar"><button>Entrar</button></div>
+                    <div className = "home"><button class = "btn">Home</button></div>
+                    <div className = "store"><button class = "btn">Store</button></div>
+                    <div className = "biblioteca"><button class = "btn">Biblioteca</button></div>
+                    <div className = "entrar"><button class = "btn" >Entrar</button></div>
                 </div>
-                
-        </div>
+            </div>
+            
         </div>
 
         
