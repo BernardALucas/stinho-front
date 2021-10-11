99a640e9577cf65fab946aa21ee6b956360fa591
Leonardo Borges <beolorges@gmail.com>
2021-10-11 11:58:49 -0300

    Criação de Rotas

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./css/Home.css"
+
+function Home(){
+    return(
+        <div header> 
+            <h1> Hello World </h1>
+        </div>
+
+    );
+}
+
+export default Home;
