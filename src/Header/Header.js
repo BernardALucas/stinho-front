121e03d149303c8d9ac764dd7219f4346c07790b
Leonardo Borges <beolorges@gmail.com>
2021-10-13 19:35:38 -0300

    inicio front end home

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,16 +6,16 @@ function Header(){
     const history = useHistory();
 
     return(   
-            <div className = "base">
-                <div className="logo">
+            <div className = "base-header">
+                <div className="logo-header">
                     <img src="./images/logo_header.png" alt= "Stinho"/> 
                 </div>
 
-                <div className = "botoes">
-                    <div className = "home"><button onClick = {()=>{history.push("home");}} class = "btn">Home</button></div>
-                    <div className = "store"><button onClick = {()=>{history.push("store");}} class = "btn">Store</button></div>
-                    <div className = "biblioteca"><button onClick = {()=>{history.push("biblioteca");}} class = "btn">Biblioteca</button></div>
-                    <div className = "entrar"><button onClick = {()=>{history.push("login");}} class = "btn" >Entrar</button></div>
+                <div className = "botoes-header">
+                    <div className = "home-header"><button onClick = {()=>{history.push("home");}} class = "btn-header">Home</button></div>
+                    <div className = "store-header"><button onClick = {()=>{history.push("store");}} class = "btn-header">Store</button></div>
+                    <div className = "biblioteca-header"><button onClick = {()=>{history.push("biblioteca");}} class = "btn-header">Biblioteca</button></div>
+                    <div className = "entrar-header"><button onClick = {()=>{history.push("login");}} class = "btn-header" >Entrar</button></div>
                 </div>
             </div>           
     )
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,8 +4,75 @@ import "./Home.css"
 
 function Home(){
     return(
-        <div header> 
+        <div className = "container-home"> 
             { Header() }
+
+            <div className = "destaques-home"> 
+                <div className = "titulo-home"><h1> Destaques </h1></div>
+                <div className = "carrossel-home">
+                    <img src="./images/new_world_logo.png" alt= "New World"></img>
+                </div>
+
+            </div>
+
+            <div className = "promocao-home">
+                <div className = "titulo-home"><h1> Jogos em Promoção </h1></div>
+                <div className = "promocao-destaque-home">
+
+                    <div className = "destaque1-home">
+                        <img className = "image1-home" src="./images/new_world_logo.png" alt= "New World"></img>
+
+                        <div className = "texto-promocao-destaque-home">
+                            <div className = "texto-promocao-destaque-jogo1">
+                                <h1>Hades – Lute para sair do inferno</h1>
+                                <h2>Supergiant Games</h2>
+                                
+                                <div className ="preco">
+                                    <h2><s> R$69,99 </s></h2>
+                                    <h1>R$29,99</h1>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className = "destaque2-home">
+                        <img className = "image1-home" src="./images/new_world_logo.png" alt= "New World"></img>
+
+                        <div className = "texto-promocao-destaque-home">
+                            <div className = "texto-promocao-destaque-jogo1">
+                                <h1>Hades – Lute para sair do inferno</h1>
+                                <h2>Supergiant Games</h2>
+                                
+                                <div className ="preco">
+                                    <h2><s> R$69,99 </s></h2>
+                                    <h1>R$29,99</h1>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    
+                    {/* <div className = "imagem-promocao-destaque-home">
+                        <img className = "image1-home" src="./images/new_world_logo.png" alt= "New World"></img>
+                        <img src="./images/new_world_logo.png" alt= "New World"></img>
+                    </div>
+                    <div className = "texto-promocao-destaque-home">
+                        <div className = "texto-promocao-destaque-jogo1">
+                            <h1>Hades – Lute para sair do inferno</h1>
+                            <h2>Supergiant Games</h2>
+                            
+                            <div className ="preco">
+                                <h2><s> R$69,99 </s></h2>
+                                <h1>R$29,99</h1>
+                            </div>
+
+                        </div>
+                    </div> */}
+                </div>
+            </div>
+
+
         </div>
 
     );
