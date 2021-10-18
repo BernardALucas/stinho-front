b80eef984831e612c2d0d5c7c94e8936b65d8230
bernard <bernardlucas@cpejr.com.br>
2021-10-18 17:17:00 -0300

    cadastro
    front-end do cadastro pronto e adição de um scrollbar estilizado

--- src/Pages/Cadastro/Cadastro.js
+++ src/Pages/Cadastro/Cadastro.js
@@ -1,8 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import "./cadastro.css"
+import { useHistory } from "react-router-dom"
 
-function Cadastro(){
-    return(
-        <h1>Cadastro</h1>
+    function Cadastro(){
+        const history = useHistory();
+        const [email, setEmail]=useState();
+        const [password, setPassword]=useState();
+        return(
+        <div className="base-cadastro"> 
+            <div className="retangulo-cadastro">
+                <div className="logo-cadastro">
+                <img src="/images/logo.png" alt="logo"/>
+                </div> 
+
+                <div className="input-cadastro">
+                <input className="caixa-cadastro" placeholder="Endereço de E-mail"></input>
+                <input className="caixa-cadastro" placeholder="Nome de perfil"></input>
+                <input className="caixa-cadastro" placeholder="Senha"></input>
+                <input className="caixa-cadastro" placeholder="Confirmar senha"></input>
+                <input className="caixa-cadastro" placeholder="Numero de telefone"></input>
+                
+                <div>
+                <input className="caixa-menor" placeholder="Estado"></input>
+                <input className="caixa-media" placeholder="Cidade"></input>
+                </div>
+
+                <input className="caixa-cadastro" placeholder="bairro"></input>
+                
+                <div>
+                <input className="caixa-menor" placeholder="N°da casa"></input>
+                <input className="caixa-media" placeholder="Rua"></input>
+                </div>
+                
+                {/* <div className="enviar-email">
+                <input type="checkbox" id="checkbox-login"className="checkbox-cadastro" />
+                <label for="checkbox-login" className="enviar-cadastro"> por favor me envie emails que vão encher a minha caixa de entrada  </label>
+                </div> */}
+
+                <button className="botao-cadastrar-se"> criar uma conta</button>
+                <div className="entrar-cadastro">
+                    já é cadastrado?
+                    <button className="btn-cadastro" onClick = {()=>{history.push("login");}}>entrar</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
 
     );
 }
