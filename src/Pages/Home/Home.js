1ff526429ec8e713025315577d047a6a9038ba44
Leonardo Borges <beolorges@gmail.com>
2021-10-18 21:46:34 -0300

    Início Carrossel
    Falta fazer ficar infinito
    Colocar clicks no jogo dnv

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,17 +1,76 @@
 import React from "react";
+import { useHistory } from "react-router";
 import Header from "../../Header";
 import "./Home.css"
+import { useRef, useState, useEffect } from "react";
+
+
 
 function Home(){
+    const history = useHistory();
+    const carrossel = useRef(null);
+
+    const HandleLeftClick = (e) => {
+        carrossel.current.scrollLeft -= carrossel.current.offsetWidth;
+    }
+
+    const HandleRightClick = (e) => {
+        /* e.preventDefault(); */
+        carrossel.current.scrollLeft += carrossel.current.offsetWidth;
+
+    }
+
     return(
         <div className = "container-home"> 
             { Header() }
 
             <div className = "destaques-home"> 
-                <div className = "titulo-home"><h1> Destaques </h1></div>
-                <div className = "carrossel-home">
-                    <img src="./images/new_world_logo.png" alt= "New World"></img>
+                <div className = "titulo-home">
+                    <h1> Destaques </h1>
                 </div>
+
+                <div className = "carrossel_titulo">
+                        <div className = "carrossel-home">
+                            <div id = "items_wrapper_home">
+                                <div id = "items_home" ref = {carrossel}>
+                                    <button onClick = {()=>{history.push("jogo1");}}>
+                                        <img src="./images/new_world_logo.png" alt= "New World"></img>
+                                    </button>
+                                    
+                                    <button onClick = {()=>{history.push("jogo2");}}>
+                                        <img src="./images/new_world_logo.png" alt= "New World"></img>
+                                    </button>
+                                    
+                                    <button onClick = {()=>{history.push("jogo3");}}>
+                                        <img src="./images/new_world_logo.png" alt= "New World"></img>
+                                    </button>
+                                    
+                                    <button onClick = {()=>{history.push("jogo4");}}>
+                                        <img src="./images/new_world_logo.png" alt= "New World"></img>
+                                    </button>
+                                
+                                    <button onClick = {()=>{history.push("jogo5");}}>
+                                        <img src="./images/new_world_logo.png" alt= "New World"></img>
+                                    </button>
+
+                                </div>
+                                
+                            </div>
+                        
+                    </div>
+
+                    <div className = "botao_passar_pro_lado">
+                        <button className = "botao_left" onClick = {()=> {HandleLeftClick()}}>
+                            <img src="./images/216151_right_chevron_icon.png" alt = "Botao Left"/> 
+                        </button>
+
+                        <button className = "botao_right" onClick = {()=> {HandleRightClick()}}>
+                            <img src="./images/216151_right_chevron_icon.png" alt = "Botao Right"/>
+                        </button>
+                    </div>
+
+                </div>
+                
 
             </div>
 
@@ -21,7 +80,7 @@ function Home(){
                 
                 {/* Divisão dos dois jogos em destaque da parte de promoções */}
                 <div className = "promocao-destaque-home">
-                    <div className = "destaque1-home">
+                    <button className = "destaque1-home" onClick = {()=>{history.push("jogo6");}}>
                         <img className = "image1-home" src="./images/new-world-esticado.png" alt= "New World"></img>
 
                         <div className = "texto-promocao-destaque-home">
@@ -31,7 +90,7 @@ function Home(){
                                 
                                 <div className = "conjunto-preco-destaque">
                                     
-                                    <div className = "desconto">
+                                    <div className = "desconto-home">
                                         <h1>- 57%</h1>
                                     </div>
 
@@ -43,9 +102,9 @@ function Home(){
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </button>
 
-                    <div className = "destaque2-home">
+                    <button className = "destaque2-home" onClick = {()=>{history.push("jogo7");}}>
                         <img className = "image2-home" src="./images/new-world-esticado.png" alt= "New World"></img>
 
                         <div className = "texto-promocao-destaque-home">
@@ -55,7 +114,7 @@ function Home(){
                                 
                                 <div className = "conjunto-preco-destaque">
                                     
-                                    <div className = "desconto">
+                                    <div className = "desconto-home">
                                         <h1> - 57%</h1>
                                     </div>
 
@@ -68,15 +127,15 @@ function Home(){
 
                             </div>
                         </div>
-                    </div>
+                    </button>
         
                 </div>
 
                 <div className = "todos-jogos-promocao">
                     <div className ="coluna1-home">
-                        <div className = "jogo1-promocao-home">
+                        <button className = "jogo1-promocao-home" onClick = {()=>{history.push("jogo8");}}>
                             <div className = "imagem-jogo1-home">
-                                <div className="desconto"><h1>- 49%</h1></div>
+                                <div className="desconto-home"><h1>- 49%</h1></div>
                             </div>
 
                             <div className = "texto-jogo1-home">
@@ -90,11 +149,11 @@ function Home(){
                                 </div>
 
                             </div>
-                        </div>
+                        </button>
 
-                        <div className = "jogo1-promocao-home">
+                        <button className = "jogo1-promocao-home" onClick = {()=>{history.push("jogo9");}}>
                             <div className = "imagem-jogo1-home">
-                                <div className="desconto"><h1>- 49%</h1></div>
+                                <div className="desconto-home"><h1>- 49%</h1></div>
                             </div>
 
                             <div className = "texto-jogo1-home">
@@ -108,11 +167,11 @@ function Home(){
                                 </div>
 
                             </div>
-                        </div>
+                        </button>
 
-                        <div className = "jogo1-promocao-home">
+                        <button className = "jogo1-promocao-home" onClick = {()=>{history.push("jogo10");}}>
                             <div className = "imagem-jogo1-home">
-                                <div className="desconto"><h1>- 49%</h1></div>
+                                <div className="desconto-home"><h1>- 49%</h1></div>
                             </div>
 
                             <div className = "texto-jogo1-home">
@@ -126,13 +185,13 @@ function Home(){
                                 </div>
 
                             </div>
-                        </div>
+                        </button>
                     </div>
 
                     <div className ="coluna2-home">
-                        <div className = "jogo1-promocao-home">
+                        <button className = "jogo1-promocao-home" onClick = {()=>{history.push("jogo11");}}>
                             <div className = "imagem-jogo1-home">
-                                <div className="desconto"><h1>- 49%</h1></div>
+                                <div className="desconto-home"><h1>- 49%</h1></div>
                             </div>
 
                             <div className = "texto-jogo1-home">
@@ -146,11 +205,11 @@ function Home(){
                                 </div>
 
                             </div>
-                        </div>
+                        </button>
                         
-                        <div className = "jogo1-promocao-home">
+                        <button className = "jogo1-promocao-home" onClick = {()=>{history.push("jogo12");}}>
                             <div className = "imagem-jogo1-home">
-                                <div className="desconto"><h1>- 49%</h1></div>
+                                <div className="desconto-home"><h1>- 49%</h1></div>
                             </div>
 
                             <div className = "texto-jogo1-home">
@@ -164,11 +223,11 @@ function Home(){
                                 </div>
 
                             </div>
-                        </div>
+                        </button>
 
-                        <div className = "jogo1-promocao-home">
+                        <button className = "jogo1-promocao-home" onClick = {()=>{history.push("jogo13");}}>
                             <div className = "imagem-jogo1-home">
-                                <div className="desconto"><h1>- 49%</h1></div>
+                                <div className="desconto-home"><h1>- 49%</h1></div>
                             </div>
 
                             <div className = "texto-jogo1-home">
@@ -182,19 +241,20 @@ function Home(){
                                 </div>
 
                             </div>
-                        </div>
+                        </button>
                     </div>
                 </div>
 
             </div>
             
             <div className = "botao-home">
-                <button>TODOS OS JOGOS</button>
+                <button onClick = {()=>{history.push("todos-os-jogos");}}>TODOS OS JOGOS</button>
             </div>
 
         </div>
 
     );
 }
+
 
 export default Home;
