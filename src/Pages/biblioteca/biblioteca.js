5aaa0c93ec2550a4a3c14c4ebdb5bb55ed40542b
bernard <bernardlucas@cpejr.com.br>
2021-10-22 13:52:32 -0300

    biblioteca né

--- src/Pages/biblioteca/biblioteca.js
+++ src/Pages/biblioteca/biblioteca.js
@@ -9,8 +9,15 @@ function biblioteca(){
     return(
         <div>
             { Header() }
-            <div>
-            
+            <div classname="base-biblioteca">
+                <div className="img-biblioteca">
+                    <img src="/images/new_world_logo.png" alt="new_world_logo"/>
+
+                </div>
+                <div className="img-biblioteca">
+                    <img src="/images/new_world_logo.png" alt="new_world_logo"/>
+
+                </div>
             </div>
             {Footer()}
         </div>
