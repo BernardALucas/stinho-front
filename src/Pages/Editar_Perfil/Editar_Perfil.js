ce941ebbbcb0455808a8e92d20d78d1566d623c3
Mateus <mateuspardini6@gmail.com>
2021-10-25 17:31:48 -0300

    Algumas Mudanças Perfil Editar Perfil

--- src/Pages/Editar_Perfil/Editar_Perfil.js
+++ src/Pages/Editar_Perfil/Editar_Perfil.js
@@ -22,22 +22,18 @@ function Editar_Perfil() {
         <div className="Parte_de_Baixo_Editar_Perfil">
           <div className="Lado_Esquerdo_Editar_Perfil">
             <div className="Dados_Esquerda_Editar_Perfil">
-              <div className="Escrita_Esquerda">Nome:</div>
-
               <input
-                className="Text_Box_Editar_Perfil"
-                placeholder="Rêlampago Marquinhos"
+                className="Text_Box_Nome_Editar_Perfil"
+                placeholder="Nome:"
               ></input>
 
-              <div className="Escrita_Esquerda">Resumo:</div>
-
               <input
-                className="Text_Box_Editar_Perfil"
-                placeholder="Foguete não tem ré nem eu"
+                className="Text_Box_Resumo_Editar_Perfil"
+                placeholder="Resumo:"
               ></input>
             </div>
             <div className="Mudar_Foto_Perfil">
-              <div className="Escrita_Esquerda">Avatar:</div>
+              <div className="Escrita_Avatar">Avatar:</div>
 
               <button className="Mudar_Avatar"> Enviar foto</button>
             </div>
@@ -48,83 +44,71 @@ function Editar_Perfil() {
             <div className="Dados_Direita_Perfil">
               <div className="Escrita_Direita">Dados de Cobrança:</div>
               <div className="Mudar_Email">
-                <div className="Escrita_Direita_Inferior">Email:</div>
                 <div className="Caixa_Mudança">
                   <input
-                    className="Text_Box_Editar_Perfil"
-                    placeholder="relampagomarquinhos@cpejr.com.br"
+                    className="Text_Box_Email_Editar_Perfil"
+                    placeholder="E-mail:"
                   ></input>
                 </div>
               </div>
               <div className="Mudar_Telefone">
-                <div className="Escrita_Direita_Inferior">Telefone:</div>
-                <div className="Caixa_Mudança">
-                  <input
-                    className="Text_Box_Editar_Perfil"
-                    placeholder="(31) 99999-9999"
-                  ></input>
-                </div>
+                <input
+                  className="Text_Box_Telefone_Editar_Perfil"
+                  placeholder="Telefone:"
+                ></input>
               </div>
               <div className="Barra_Verde_Estética"></div>
 
               <div className="Escrita_Direita">Endereço:</div>
               <div className="País_Estado">
-                <div className="Mudar_País">
-                  <div className="Escrita_Direita_Inferior">País:</div>
-                  <div className="Caixa_Mudança">
-                    <input
-                      className="Text_Box_Editar_Perfil"
-                      placeholder="Brasil"
-                    ></input>
-                  </div>
-                </div>
-                <div className="Mudar_Estado">
-                  <div className="Escrita_Direita_Inferior">Estado:</div>
-                  <div className="Caixa_Mudança">
-                    <input
-                      className="Text_Box_Editar_Perfil"
-                      placeholder="Minas Gerais"
-                    ></input>
-                  </div>
-                </div>
-              </div>
-              <div className="Mudar_Cidade">
-                <div className="Escrita_Direita_Inferior">Cidade:</div>
                 <div className="Caixa_Mudança">
                   <input
-                    className="Text_Box_Editar_Perfil"
-                    placeholder="Belo Horizonte"
+                    className="Text_Box_País_Editar_Perfil"
+                    placeholder="País:"
+                  ></input>
+                </div>
+                <div className="Caixa_Mudança">
+                  <input
+                    className="Text_Box_Estado_Editar_Perfil"
+                    placeholder="Estado:"
                   ></input>
                 </div>
               </div>
-              <div className="Mudar_Bairro">
-                <div className="Escrita_Direita_Inferior">Bairro:</div>
-                <div className="Caixa_Mudança">
-                  <input
-                    className="Text_Box_Editar_Perfil"
-                    placeholder="Centro"
-                  ></input>
-                </div>
-              </div>
+              <div className="Cidade_Bairro">
+                <input
+                  className="Text_Box_Cidade_Editar_Perfil"
+                  placeholder="Cidade:"
+                ></input>
 
+                <div className="Mudar_Bairro">
+                  <div className="Escrita_Direita_Inferior"></div>
+                  <div className="Caixa_Mudança">
+                    <input
+                      className="Text_Box_Bairro_Editar_Perfil"
+                      placeholder="Bairro:"
+                    ></input>
+                  </div>
+                </div>
+              </div>
+              <div className="Rua_Numero">
               <div className="Mudar_Rua">
-                <div className="Escrita_Direita_Inferior">Rua:</div>
                 <div className="Caixa_Mudança">
                   <input
-                    className="Text_Box_Editar_Perfil"
-                    placeholder="Afonso Pena"
+                    className="Text_Box_Rua_Editar_Perfil"
+                    placeholder="Rua:"
                   ></input>
                 </div>
               </div>
               <div className="Mudar_Numero">
-                <div className="Escrita_Direita_Inferior">Número:</div>
+                <div className="Escrita_Direita_Inferior"></div>
                 <div className="Caixa_Mudança">
                   <input
-                    className="Text_Box_Editar_Perfil"
-                    placeholder="666"
+                    className="Text_Box_Numero_Editar_Perfil"
+                    placeholder="Número:"
                   ></input>
                 </div>
               </div>
+            </div>
             </div>
             <div className="Barra_Verde_Estética"></div>
             <button className="Botão_Confirmar_Alterações">
