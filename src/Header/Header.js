302b0cb97f230addd23ad48a0811fd58e28293cb
Mateus <mateuspardini6@gmail.com>
2021-11-02 17:29:45 -0300

    Login funcional marromenos

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,41 +1,117 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Header.css";
-import Dropdown from 'react-bootstrap/Dropdown';
+import Dropdown from "react-bootstrap/Dropdown";
 import { CgMenu } from "react-icons/cg";
+import {FiLogOut} from "react-icons/fi";
 
 function Header() {
+  const [label, setLabel] = useState("");
+  const user = JSON.parse(localStorage.getItem("user"));
+  useEffect(() => {
+    console.log(user);
+    user ? setLabel("PERFIL") : setLabel("ENTRAR");
+  },[user]);
 
-    return(   
-        
-            <div>
-            <div className = "base-header">
-                <div className="logo-header">
-                    <img src="./images/logo_header.png" alt= "Stinho"/> 
-                </div>
+  return (
+    <div>
+      <div className="base-header">
+        <div className="logo-header">
+          <img src="./images/logo_header.png" alt="Stinho" />
+        </div>
 
-                <div className = "botoes-header">
-                    <div className = "home-header"><button onClick = {()=>{window.location.href = "/home";}} class = "btn-header">HOME</button></div>
-                    <div className = "store-header"><button onClick = {()=>{window.location.href = "/allgames";}} class = "btn-header">STORE</button></div>
-                    <div className = "biblioteca-header"><button onClick = {()=>{window.location.href = "/biblioteca";}} class = "btn-header">BIBLIOTECA</button></div>
-                    <div className = "entrar-header"><button onClick = {()=>{window.location.href = "/login";}} class = "btn-header" >ENTRAR</button></div>
-                </div>
-            <div className ="btn-drawer-header">
-                    <Dropdown className="menu-header" style={{backgroundColor:"black"}} >
-                        <Dropdown.Toggle id="dropdown-button-dark-example1" variant="dark"style={{backgroundColor:"black"}}>< CgMenu /></Dropdown.Toggle>
-                        <Dropdown.Menu variant="dark" className="super-colors" style={{backgroundColor:"#000000"}} >
-                        <Dropdown.Item onClick = {()=>{window.location.href = "/home";}}  >HOME</Dropdown.Item>
-                        <Dropdown.Item onClick = {()=>{window.location.href = "/allgames";}} >STORE</Dropdown.Item>
-                        <Dropdown.Item onClick = {()=>{window.location.href = "/biblioteca";}}>BIBLIOTECA</Dropdown.Item>
-                        <Dropdown.Item onClick = {()=>{window.location.href = "/login";}}>ENTRAR</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </div>
-        
-            </div>
-
-            </div>     
-    )
-    
+        <div className="botoes-header">
+          <div className="home-header">
+            <button
+              onClick={() => {
+                window.location.href = "/";
+              }}
+              class="btn-header"
+            >
+              HOME
+            </button>
+          </div>
+          <div className="store-header">
+            <button
+              onClick={() => {
+                window.location.href = "/allgames";
+              }}
+              class="btn-header"
+            >
+              STORE
+            </button>
+          </div>
+          <div className="biblioteca-header">
+            <button
+              onClick={() => {
+                window.location.href = "/biblioteca";
+              }}
+              class="btn-header"
+            >
+              BIBLIOTECA
+            </button>
+          </div>
+          <div className="entrar-header">
+            <button
+              onClick={() => {
+                window.location.href = "/login";
+              }}
+              class="btn-header"
+            >
+              {label}
+            </button>
+          </div>
+        </div>
+        <div className="btn-drawer-header">
+          <Dropdown
+            className="menu-header"
+            style={{ backgroundColor: "black" }}
+          >
+            <Dropdown.Toggle
+              id="dropdown-button-dark-example1"
+              variant="dark"
+              style={{ backgroundColor: "black" }}
+            >
+              <CgMenu />
+            </Dropdown.Toggle>
+            <Dropdown.Menu
+              variant="dark"
+              className="super-colors"
+              style={{ backgroundColor: "#000000" }}
+            >
+              <Dropdown.Item
+                onClick={() => {
+                  window.location.href = "/";
+                }}
+              >
+                HOME
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => {
+                  window.location.href = "/allgames";
+                }}
+              >
+                STORE
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => {
+                  window.location.href = "/biblioteca";
+                }}
+              >
+                BIBLIOTECA
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => {
+                  window.location.href = "/login";
+                }}
+              >
+                {label}
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,57 +1,95 @@
 import React, { useState } from "react";
 import "./Login.css";
 import Footer from "../../Footer";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useHistory } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useHistory } from "react-router-dom";
+import api from "../../services/api";
+import { BsFillCreditCard2FrontFill, BsListNested } from "react-icons/bs";
 
-function Login(){
-    const history = useHistory();
-    const [email, setEmail]=useState();
-    const [password, setPassword]=useState();
+function Login() {
+  const history = useHistory();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-    async function login(e){
-        e.preventDefault();
-        history.push("home")
-    }
+  async function login(e) {
+    e.preventDefault();
+    api
+      .post("/login", { email, password })
+      .then((res) => {
+        localStorage.setItem("user", JSON.stringify(res.data));
+        history.push("/");
+      })
+      .catch((err) => console.error(err));
+  }
 
-    return(
-        <div className="page-login">
-        <div className="base-login">
-            <div className="retangulo-login">
-                <div className="voltar-login">
-                <button className="btn-login" onClick = {()=>{history.push("home");}} > voltar para a pagina principal</button>
-                </div>
+  return (
+    <div className="page-login">
+      <div className="base-login">
+        <div className="retangulo-login">
+          <div className="voltar-login">
+            <button
+              className="btn-login"
+              onClick={() => {
+                history.push("/");
+              }}
+            >
+              {" "}
+              voltar para a pagina principal
+            </button>
+          </div>
 
-                <div className="conjunto-login">
-                    <div className="logo-login">
-                    <img src="/images/logo.png" alt="logo"/>
-                    </div>
-                    <div className="inputs-login">
-                        <input className="email-login" placeholder="endereço de email" onChange={(e)=> setEmail(e.target.value)}></input>
-                        <input className="email-login" placeholder="senha" type="password" onChange={(e)=> setPassword(e.target.value)}></input>
-                        <div className="conjutno-checkbox-login">
-                            <input type="checkbox" id="checkbox-login"className="checkbox-login" />
-                            <label for="checkbox-login" className="manter-logado"> manter-me logado </label>
-                            <button className="btn-login">esqueceu sua senha</button>
-                        </div>
-                        <div className="botao-logar"><button className="btn-logar" onClick={login}>iniciar sessão</button></div>
-                    </div>
-                    <div className="cadastrar-login">
-                    ainda não é cadastrado?
-                    <button className="btn-login" onClick = {()=>{history.push("cadastro");}}>cadastrar-se</button>
-                    </div>
-
-                </div>  
+          <div className="conjunto-login">
+            <div className="logo-login">
+              <img src="/images/logo.png" alt="logo" />
             </div>
-
+            <div className="inputs-login">
+              <input
+                className="email-login"
+                placeholder="endereço de email"
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+              <input
+                className="email-login"
+                placeholder="senha"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                
+              ></input>
+              <div className="conjutno-checkbox-login">
+                <input
+                  type="checkbox"
+                  id="checkbox-login"
+                  className="checkbox-login"
+                />
+                <label for="checkbox-login" className="manter-logado">
+                  {" "}
+                  manter-me logado{" "}
+                </label>
+                <button className="btn-login">esqueceu sua senha</button>
+              </div>
+              <div className="botao-logar">
+                <button className="btn-logar" onClick={login}>
+                  iniciar sessão
+                </button>
+              </div>
+            </div>
+            <div className="cadastrar-login">
+              ainda não é cadastrado?
+              <button
+                className="btn-login"
+                onClick={() => {
+                  history.push("cadastro");
+                }}
+              >
+                cadastrar-se
+              </button>
+            </div>
+          </div>
         </div>
-            { Footer() }
-        </div> 
-        
-        
-
-);
-
+      </div>
+      {Footer()}
+    </div>
+  );
 }
 
 export default Login;
