ca9a95cd79ec0aff4b7889e0f4247f57655fc1b5
Thiago Magalhães Verçosa <thvercosa@ufmg.br>
2021-11-03 15:58:11 -0300

    Responsividade AllGames pronta

--- src/Pages/All_Games/All_Games.js
+++ src/Pages/All_Games/All_Games.js
@@ -2,76 +2,126 @@ import React, { useEffect, useState } from "react";
 import Header from "../../Header";
 import Footer from "../../Footer";
 import "./All_Games.css";
-import Dropdown from 'react-bootstrap/Dropdown';
+import Dropdown from "react-bootstrap/Dropdown";
 
+function All_Games() {
+  const [data, setData] = useState([]);
+  const [filtro, setFiltro] = useState([]);
 
+  useEffect(() => {
+    fetch("http://localhost:3000/jogos.json")
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
 
-function All_Games(){
-    const [data, setData] = useState([]);
-    const [filtro, setFiltro] = useState([]);
+  return (
+    <div>
+      {Header()}
+      <div className="containerJogo">
+        <div className="filtro_all_games">
+          <Dropdown>
+            <Dropdown.Toggle
+              id="dropdown-button-dark-example1"
+              variant="secondary"
+            >
+              Filtrar
+            </Dropdown.Toggle>
+            <Dropdown.Menu variant="dark">
+              <Dropdown.Item
+                onClick={() => {
+                  setFiltro(["Todos os Jogos"]);
+                }}
+              >
+                Todos os Jogos
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => {
+                  setFiltro(["Ação"]);
+                }}
+              >
+                Ação
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => {
+                  setFiltro(["Aventura"]);
+                }}
+              >
+                Aventura
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => {
+                  setFiltro(["Estratégia"]);
+                }}
+              >
+                Estratégia
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => {
+                  setFiltro(["RPG"]);
+                }}
+              >
+                RPG
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => {
+                  setFiltro(["Tiro"]);
+                }}
+              >
+                Tiro
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
 
-    useEffect(()=>{
-        fetch('http://localhost:3000/jogos.json')
-            .then((response) => response.json())
-            .then(setData);
-    }, []);
-    
-    return(
-        <div>
-            { Header() }
-            <div className="containerJogo">
+        {data.map((jogo) => {
+          const {
+            id,
+            name,
+            price,
+            oldPrice,
+            image,
+            desenvolvedor,
+            genero,
+            descricao,
+          } = jogo;
 
-                <div className ="filtro_all_games">
-                    <Dropdown>
-                        <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">Filtrar</Dropdown.Toggle>
-                        <Dropdown.Menu variant="dark">
-                        <Dropdown.Item onClick = {()=>{setFiltro(["Todos os Jogos"])}}>Todos os Jogos</Dropdown.Item>
-                        <Dropdown.Item onClick = {()=>{setFiltro(["Ação"])}}>Ação</Dropdown.Item>
-                        <Dropdown.Item onClick = {()=>{setFiltro(["Aventura"])}}>Aventura</Dropdown.Item>
-                        <Dropdown.Item onClick = {()=>{setFiltro(["Estratégia"])}}>Estratégia</Dropdown.Item>
-                        <Dropdown.Item onClick = {()=>{setFiltro(["RPG"])}}>RPG</Dropdown.Item>
-                        <Dropdown.Item onClick = {()=>{setFiltro(["Tiro"])}}>Tiro</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
+          if (filtro.length === 0 || genero.includes(filtro[0]))
+            return (
+              <div className="seçãoJogo">
+                <img className="jogoFoto" src={image} alt={name}></img>
+
+                <div className="descriçãoJogo">
+                  <div className="textojogo">
+                    <p className="jogoTitulo"> {name}:</p>
+                    <p className="jogoDescrição"> {descricao} </p>
+                  </div>
+                  <button className="botaoVejaMais"> Veja Mais </button>
                 </div>
 
-                {data.map((jogo)=>{
-                    const {id, name, price, oldPrice, image, desenvolvedor, genero, descricao} = jogo;
+                <div className="preçoJogo">
+                  <div className="parteBranca">
+                    <p className="textoPreço"> A partir de: </p>
+                    <p className="valorAtual"> R$ {price} </p>
+                  </div>
+                  <button
+                    className="botaoComprar"
+                    onClick={() => {
+                      window.location.href = id;
+                    }}
+                  >
+                    {" "}
+                    COMPRAR{" "}
+                  </button>
+                </div>
+              </div>
+            );
+          else return null;
+        })}
+      </div>
 
-                    if(filtro.length === 0 || genero.includes(filtro[0]))
-                    return (
-                        <div className="seçãoJogo">
-                    
-                            <img className="jogoFoto" src={image} alt= {name}></img>
-                            
-                            <div className="descriçãoJogo">
-                                <div>
-                                    <p className="jogoTitulo"> {name}:</p>
-                                    <p className="jogoDescrição"> {descricao} </p>
-                                </div>
-                                <button className="botaoVejaMais"> Veja Mais </button> 
-                            </div>
-                        
-                            <div className="preçoJogo">
-                                <div>
-                                    <p className="textoPreço"> A partir de:  </p>
-                                    <p className="valorAtual"> R$ {price} </p>
-                                </div>
-                                <button className="botaoComprar" onClick ={() => { window.location.href = id; }}>   COMPRAR </button>  
-                            </div> 
-                        </div>
-                    );
-
-                    else
-                        return null;
-                })}
-            
-            </div>
-
-            { Footer() }
-        </div>
-    )
-
+      {Footer()}
+    </div>
+  );
 }
 
 export default All_Games;
