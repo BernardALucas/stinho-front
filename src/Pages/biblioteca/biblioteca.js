9f4de4f43d508223ce38742080b6e6640dfe50a3
Leonardo Borges <beolorges@gmail.com>
2021-11-04 11:25:01 -0300

    library linkada com o perfil

--- src/Pages/biblioteca/biblioteca.js
+++ src/Pages/biblioteca/biblioteca.js
@@ -2,16 +2,23 @@ import React, { useState, useRef, useEffect } from "react";
 import "./Biblioteca.css";
 import Footer from "../../Footer";
 import Header from "../../Header";
-import { useHistory } from "react-router-dom";
+import api from "../../services/api";
 
 
 function Biblioteca() {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:3000/jogos.json')
-            .then((response) => response.json())
-            .then(setData);
+        const id = localStorage.getItem("user");
+        const newId = JSON.parse(id);
+        console.log(newId.user_id);
+
+        api
+        .get(`/library/${newId.user_id}`)
+        .then((response)=>{
+            setData(response.data);
+        })
+
     }, []);
 
     return (
@@ -19,18 +26,21 @@ function Biblioteca() {
         <div className="page-biblioteca">
             {Header()}
             <div classname="base-biblioteca">
+                <div className = "titulo-biblioteca">
+                    <h1>Seus Jogos</h1>
+                </div>
                 <div className="fileira-biblioteca">
                     {data.map((iten) => {
                         console.log(data);
 
-                        const { id, name, price, oldPrice, image, desenvolvedor, genero } = iten;
+                        const { game_id, title, price, old_price, image, developer, genero, description, category, nivel_destaque } = iten;
 
                         return (
                             <div className="botao-jogos-biblioteca">
-                                <img src={image} className="img-biblioteca" alt={name} />
+                                <img src={image} className="img-biblioteca" alt={title} />
 
                                 <div className="titulo-jogo1-biblioteca">
-                                    <h1>{name}</h1>
+                                    <h1>{title}</h1>
                                 </div>
                             </div>
                         );
